bc0a027f0df40582bed0a06c0cf253a32d1791d7
Sam Byrd <sbyrd@thebyrdnest.net>
2013-07-22 18:40:24 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+  response.send("Hello3");
 });
 
 var port = process.env.PORT || 5000;
