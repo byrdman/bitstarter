07d1bf58a616c322039bab425ce02145c5b7cac2
Sam Byrd <sbyrd@thebyrdnest.net>
2013-07-22 18:43:23 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,11 @@
 var fs = require('fs');
-var buf = fs.readFileSync('index.html', function(err, data) {
-  if (err) throw err;
-  console.log(data);
-});
+var buf = fs.readFileSync('index.html');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("Hello3");
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
